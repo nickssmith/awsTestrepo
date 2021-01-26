c3165773ca1f75147cfcf4ce1e47701f8c1c4870
Nick Smith <nsmith@sflscientific.com>
2021-01-26 12:08:35 -0500

    updated service 	modified:   app.py

--- a/app.py
+++ a/app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def a():
     return 'I am route a'
 
+@app.route('/a/a')
+def a():
+    return 'I am route aa'
+    
+@app.route('/a/b')
+def a():
+    return 'I am route ab'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
